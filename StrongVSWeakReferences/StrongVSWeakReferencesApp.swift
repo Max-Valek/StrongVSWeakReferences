ac41aca54306e67b438d4cfd39c914b63881a6ec
Max Valek <valek.maxwell@gmail.com>
2023-04-10 16:57:28 -0500

    Initial Commit

---
+++ StrongVSWeakReferences/StrongVSWeakReferencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongVSWeakReferencesApp.swift
+//  StrongVSWeakReferences
+//
+//  Created by Max Valek on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StrongVSWeakReferencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
